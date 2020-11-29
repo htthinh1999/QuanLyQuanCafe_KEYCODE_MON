4be06afa4d049d57be89118ab7c7f057d7a8162d
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2020-11-29 11:52:09 +0700

    Update index page: - Update content. - Replace header with sidebar. - Add end-page to add js. - Update footer.

--- views/inc/footer.php
+++ views/inc/footer.php
@@ -1,3 +1,10 @@
-<?php
 
-?>
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>&copy; 2020 Quán cafe Keycode Mon</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
---
+++ views/inc/sidebar.php
@@ -0,0 +1,74 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-coffee"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Quán cafe KEYCODE MON</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Trang chủ</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Quản lý
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Chung</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Quản lý chung:</h6>
+                <a class="collapse-item" href="all-tables.html">Danh sách các bàn</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-user-cog"></i>
+            <span>Quản trị</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Quản lý của quản trị:</h6>
+                <a class="collapse-item" href="revenue.html">Doanh thu</a>
+                <a class="collapse-item" href="food.html">Món</a>
+                <a class="collapse-item" href="category.html">Danh mục</a>
+                <a class="collapse-item" href="table.html">Bàn</a>
+                <a class="collapse-item" href="account.html">Tài khoản</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
