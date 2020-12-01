98b68a2c35adb02625836af0827c05260287525b
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2020-12-01 11:27:26 +0700

    Index: - Remove setting at top right corner. Sidebar: - Divide Setting into 2. - Add some links.

--- views/inc/sidebar.php
+++ views/inc/sidebar.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-coffee"></i>
+            <i class="fas fa-fw fa-coffee"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Quán cafe KEYCODE MON</div>
     </a>
@@ -24,24 +24,18 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Quản lý
+        Quản lý chung
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item - Tables -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Chung</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Quản lý chung:</h6>
-                <a class="collapse-item" href="all-tables.html">Danh sách các bàn</a>
-            </div>
-        </div>
+        <a class="nav-link" href="all-tables.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Danh sách các bàn</span></a>
     </li>
 
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
     <!-- Only admin can access -->
     <?php
@@ -49,17 +43,22 @@
         if($account->getAccountTypeName() == 'Quản trị viên'){
     ?>
 
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Quản lý
+    </div>
+
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
             aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-user-cog"></i>
-            <span>Quản trị</span>
+            <i class="fas fa-fw fa-user-cog"></i>
+            <span>Quản lý</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Quản lý của quản trị:</h6>
+                <h6 class="collapse-header">Quản lý:</h6>
                 <a class="collapse-item" href="revenue.html">Doanh thu</a>
                 <a class="collapse-item" href="food.html">Món</a>
                 <a class="collapse-item" href="category.html">Danh mục</a>
@@ -75,6 +74,45 @@
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Cài đặt
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Tài khoản</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Tài khoản:</h6>
+                <a class="collapse-item" href="change-password.php">Thay đổi mật khẩu</a>
+                <a class="collapse-item" href="profile.php">Thông tin tài khoản</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Cài đặt khác</span>
+        </a>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Cài đặt khác:</h6>
+                <a class="collapse-item" href="theme.php">Thay đổi màu sắc</a>
+                <a class="collapse-item" href="backup.php">Sao lưu dữ liệu</a>
+                <a class="collapse-item" href="restore.php">Phục hồi dữ liệu</a>
+                <a class="collapse-item" href="information.php">Thông tin trang</a>
+            </div>
+        </div>
+    </li>
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
--- views/index.php
+++ views/index.php
@@ -107,11 +107,6 @@
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Hồ sơ
                                 </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Cài đặt
-                                </a>
-                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Đăng xuất
